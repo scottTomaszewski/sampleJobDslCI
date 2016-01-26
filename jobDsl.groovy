433e8de7a3e697821c7a35ba1ec2c19cc472957d
Scott Tomaszewski <scotttomaszewski@gmail.com>
2016-01-26 16:27:10 -0500

    Adds basics for building bom

--- jobDsl.groovy
+++ jobDsl.groovy
@@ -224,7 +224,6 @@ modules.each { Map module ->
     }
 }
 
-
 // Build bom with aggregate of all modules
 job(buildModulesBom) {
     description("Job for build a bom that aggregates all the latest successful releases for modules")
@@ -246,10 +245,78 @@ job(buildModulesBom) {
     }
 
     steps {
-        maven("""package
+        def script = '''
+                # prepare git
+                git config user.name "Jenkins"
+                git config user.email "DevOps_Team@FIXME.com"
+                git config push.default simple
+
+                # figure out git commit count
+                GIT_COMMIT_COUNT=`git rev-list --all --count`
+
+                # evaluate cdm version from property
+                CDM_VAR="TODO"
+
+                # evaluate declared project info
+                PROJECT_VERSION_VAR=`mvn help:evaluate -Dexpression=project.version|grep -Ev \'(^\\[|Download\\w+:)\'`
+                #PROJECT_GROUP_ID_VAR=`mvn help:evaluate -Dexpression=project.groupId|grep -Ev '(^\\[|Download\\w+:)'`
+                #PROJECT_ARTIFACT_ID_VAR=`mvn help:evaluate -Dexpression=project.artifactId|grep -Ev '(^\\[|Download\\w+:)'`
+
+                # remove "-SNAPSHOT" from project version
+                WITHOUT_SNAPSHOT=${PROJECT_VERSION_VAR%-SNAPSHOT}
+
+                # release version as MAJOR.MINOR.GIT_COMMIT_COUNT.SUFFIX
+                SEMVER="[^0-9]*\\([0-9]*\\)[.]\\([0-9]*\\)[.]\\([0-9]*\\)\\([0-9A-Za-z-]*\\)"
+                RELEASE_VER_VAR=`echo $WITHOUT_SNAPSHOT | sed -e "s#$SEMVER#\\1.\\2.${GIT_COMMIT_COUNT}\\4#"`
+
+                # next version as MAJOR.MINOR.[GIT_COMMIT_COUNT+1].SUFFIX
+                NEXT_VER_VAR=`echo $PROJECT_VERSION_VAR | sed -e "s#$SEMVER#\\1.\\2.$((GIT_COMMIT_COUNT+1))\\4#"`
+
+                # create a branch for safekeeping
+                git checkout -b staging-v$RELEASE_VER_VAR
+
+                # Add properties for EnvInject jenkins plugin
+                echo "CDM=$CDM_VAR" >> env.properties
+                echo "PROJECT_VERSION=$PROJECT_VERSION_VAR" >> env.properties
+                #echo "PROJECT_GROUP_ID=$PROJECT_GROUP_ID_VAR" >> env.properties
+                #echo "PROJECT_ARTIFACT_ID=$PROJECT_ARTIFACT_ID_VAR" >> env.properties
+                echo "RELEASE_VERSION=$RELEASE_VER_VAR" >> env.properties
+                echo "NEXT_VERSION=$NEXT_VER_VAR" >> env.properties
+
+                # print out description for Description Setter jenkins plugin
+                echo "DESCRIPTION v$RELEASE_VER_VAR (CDM=$CDM_VAR)"
+        '''
+        shell script
+
+        environmentVariables {
+            propertiesFile('env.properties')
+        }
+        buildDescription(/^DESCRIPTION\s(.*)/, '\\1')
+        wrappers {
+            buildName('#${BUILD_NUMBER} - ${GIT_REVISION, length=8} (${GIT_BRANCH})')
+        }
+
+        // set release version on poms (temp: add branchPath since using same git repo) and commit
+        maven("versions:set -DnewVersion=\'\${RELEASE_VERSION}\'")
+        shell 'git commit -am "[promote-to-staging] Bumping version to staging -> \${RELEASE_VERSION}"'
+
+        // test and deploy to nexus, then tag
+        //maven('clean install deploy -s ${SETTINGS_CONFIG} -DdeployAtEnd')
+        maven("""resources:resources deploy
+            -PbuildBom
             -Dversion.ds=0.0.37
             -Dversion.ba=0.0.16
             -Dversion.ms=0.0.10
+            -s \${SETTINGS_CONFIG}
+            -DdeployAtEnd
         """)
+        shell "git tag staging-\${RELEASE_VERSION} # TODO && git push --tags"
+
+        // switch to original branch
+        shell "git checkout -"
+
+        // increment and update to new version
+        maven("versions:set -DnewVersion=\'\${NEXT_VERSION}\'")
+        shell 'git commit -am "[promote-to-staging] Bumping after staging \${RELEASE_VERSION}. New version: \${NEXT_VERSION}" # TODO && git push'
     }
 }
